209830558d5cc5c3069927902f87769ed4e56762
Brenden Iannetta <70484211+DarkVypr@users.noreply.github.com>
2021-12-18 01:42:03 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main() {
   int loop = 0;
   while(loop == 0) {
-
-    // Ask the user to enter two numbers.
     int num1, num2;
     
+    // Ask the user to enter two numbers.
+      
     cout << "Please enter the first number: \n";
     cin >> num1;
-    
+      
     cout << "Please enter the second number: \n";
     cin >> num2;
-    
+      
     // Check the numbers, and respond to the user appropriately.
     if(num1 > num2) {
       cout << "The number " << num1 << " is bigger than " << num2 << ".\n";
     }
-    else if(num2 < num1) {
+    else if(num2 > num1) {
       cout << "The number " << num2 << " is bigger than " << num1 << ".\n";
     }
     else {
